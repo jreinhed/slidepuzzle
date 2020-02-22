aa2309e6a857214a9d65450a4bdc3bb982c7bace
Johan Reinhed <jreinhed@protonmail.com>
2020-02-22 14:47:51 +0100

    Event handlers, win condition

--- Grid.java
+++ Grid.java
@@ -6,21 +6,30 @@ import java.util.*;
 public class Grid implements ActionListener {
 	private JButton[] button;
 	private int size;
+	private int clicks;
+	private String correctOrder;
 
 	public Grid(int newSize, int buttonSize) {
+		clicks = 0;
 		size = newSize * newSize;
 		button = new JButton[size];
+		correctOrder = "";
+		
+		for (int i = 1; i < size; i++) {
+			correctOrder += i + " ";
+		}
+		correctOrder += " ";
 
 		for (int i = 0; i < button.length; i++) {
 			button[i] = new JButton();
 		}
 
 		UniqueRNG urng = new UniqueRNG(size);
-		Integer[] i = urng.getNumList();
+		Integer[] integers = urng.getNumList();
+
 		int x = buttonSize;
 		int y = buttonSize;
 		int count = 0;
-
 		int n = 0;
 
 		for (JButton b : button) {
@@ -33,23 +42,76 @@ public class Grid implements ActionListener {
 				x = buttonSize;
 			}
 
-			b.setText(i[n].toString());
+			// We only want numbers in the range [1, size)
+			if (integers[n] == size) {
+				b.setText("");
+			} else {
+				b.setText(integers[n].toString());
+			}
+
 			n++;
 			b.addActionListener(this);
 		}
+	}
 
-		button[size - 1].setText("");
+	private void switchButtons(JButton[] buttons, int i, int offset) {
+		if (button[i+offset].getText() == "") {
+			button[i+offset].setText(button[i].getText());
+			button[i].setText("");
+		}
 	}
 
 	public void actionPerformed(ActionEvent e) {
 		Object obj = e.getSource();
+		String buttonOrder = "";
+		int sideSize = (int) Math.sqrt(size); // Size of one side
 
-		for (JButton b : button) {
-			if (obj == b) {
-				System.out.println("This one!");
-				break;
+		for (int i = 0; i < size; i++) {
+			if (obj == button[i]) {
+				clicks++;
+
+				// Horizontal
+				if (i > 0) {
+					switchButtons(button, i, -1);
+					break;
+				}
+
+				if (i < size - 1) {
+					switchButtons(button, i, 1);
+					break;
+				}
+
+				// Vertical
+				if (i >= sideSize) {
+					switchButtons(button, i, -sideSize);
+					break;
+				}
+
+				if (i < size - sideSize) {
+					switchButtons(button, i, sideSize);
+					break;
+				}
 			}
 		}
+
+		for (JButton b : button) {
+			buttonOrder += b.getText() + " ";
+		}
+
+		if (buttonOrder.equals(correctOrder)) {
+			win();
+		}
+	}
+
+	private void win() {
+		System.out.println("Congratulations! You won after " + clicks + " clicks.");
+
+		if (clicks < 0) {
+			System.out.println("Wait... " + clicks + "?");
+			System.out.println("What did you do?");
+		}
+
+		System.exit(0);
 	}
 
 	public void draw(JFrame frame) {
--- UniqueRNG.java
+++ UniqueRNG.java
@@ -2,26 +2,24 @@ import java.util.Random;
 import java.util.List;
 import java.util.ArrayList;
 
-class UniqueRandomNumberGenerator {
+class UniqueRNG {
 	private Integer[] numList;
 
 	public Integer[] getNumList() {
 		return numList;
 	}
 
-	public UniqueRandomNumberGenerator(int num) {
+	public UniqueRNG(int num) {
 		numList = new Integer[num];
 		Random rand = new Random();
 		List<Integer> used = new ArrayList<Integer>();
 
-		for (int i = 0; i < num - 1; i++) {
+		for (int i = 0; i < num; i++) {
 			do {
-				numList[i] = rand.nextInt(num - 1) + 1;
+				numList[i] = rand.nextInt(num) + 1;
 			} while (used.contains(numList[i]));
 
 			used.add(numList[i]);
 		}
-
-		numList[num - 1] = 34;
 	}
 }
